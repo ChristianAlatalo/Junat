6a9ba93ba0859f0d9712a85843400d23acc71391
saoltupp <saoltupp@gmail.com>
2019-06-26 16:52:18 +0300

    Tehty metodi matkallaTurkuun() ja luotu luokka Locations

---
+++ src/fi/academy/Location.java
@@ -0,0 +1,16 @@
+package fi.academy;
+
+import java.util.List;
+
+public class Location {
+    String type;
+    List<Double> coordinates;
+
+    public String getType() {
+        return this.type;
+    }
+
+    public List getCoordinates() {
+        return this.coordinates;
+    }
+}
